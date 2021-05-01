8989d21d2f9acf19092885783a8ecbc7cfbc31ef
unknown <Mostafa Hares>
2021-05-01 23:15:39 +0200

    Spring bean with default bean id

--- src/spring/annotations/config/AnnotationsDemoApp.java
+++ src/spring/annotations/config/AnnotationsDemoApp.java
@@ -11,7 +11,7 @@ public class AnnotationsDemoApp {
 				           new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// get the bean from spring container
-		Coach theCoach = context.getBean("thatSillyCoach",Coach.class );
+		Coach theCoach = context.getBean("tennisCoach",Coach.class );
 		
 		// call method on the bean 
 		System.out.println(theCoach.getDailyWorkout());
--- src/spring/annotations/config/TennisCoach.java
+++ src/spring/annotations/config/TennisCoach.java
@@ -2,7 +2,7 @@ package spring.annotations.config;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
