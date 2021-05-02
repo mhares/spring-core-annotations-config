1a80248168acbfb4d8f272ac68b87d69b7a2f1b6
unknown <Mostafa Hares>
2021-05-02 04:44:48 +0200

    IOC & DI full java code

--- src/spring/annotations/config/JavaConfigDemoApp.java
+++ src/spring/annotations/config/JavaConfigDemoApp.java
@@ -1,14 +1,15 @@
 package spring.annotations.config;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 public class JavaConfigDemoApp {
 
 	public static void main(String[] args) {
 	
-		// read spring configuration file 
-		ClassPathXmlApplicationContext context =
-				           new ClassPathXmlApplicationContext("applicationContext.xml");
+		// read spring configuration class 
+	AnnotationConfigApplicationContext context =
+				           new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		// get the bean from spring container
 		Coach theCoach = context.getBean("tennisCoach",Coach.class );
--- src/spring/annotations/config/SportConfig.java
+++ src/spring/annotations/config/SportConfig.java
@@ -1,10 +1,28 @@
 package spring.annotations.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("spring.annotations.config")
-public class SportConfig {
 
+public class SportConfig {
+	
+	// define bean for our sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	// define bean for our swim coach AND inject dependency
+	@Bean
+	public Coach swimCoach() {
+		SwimCoach mySwimCoach = new SwimCoach(sadFortuneService());
+		
+		return mySwimCoach;
+	}
+	
 }
+
+
